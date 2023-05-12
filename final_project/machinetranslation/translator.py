c6081465c3fd81f4657fd90e00f50a8d558f08f2
MutantDreams <nathaniel.s.parrish@gmail.com>
2023-05-11 23:57:37 -0400

    Final changes

---
+++ final_project/machinetranslation/translator.py
@@ -0,0 +1,43 @@
+"""All functions and imports for the translation"""
+
+import json
+import os
+from ibm_watson import LanguageTranslatorV3
+from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
+from dotenv import load_dotenv
+
+
+load_dotenv()
+
+apikey = os.environ['apikey']
+url = os.environ['url']
+
+
+
+
+#Create instance of translator
+authenticator = IAMAuthenticator(apikey)
+language_translator = LanguageTranslatorV3(
+    version='2018-05-01',
+    authenticator=authenticator
+)
+
+language_translator.set_service_url(url)
+language_translator.set_disable_ssl_verification(True)
+
+
+#Functions
+
+def english_to_french(english_text):
+    """Convert English to French"""
+    french_text = language_translator.translate(
+        text=english_text,
+        model_id='en-fr').get_result()
+    return french_text['translations'][0]['translation']
+
+def french_to_english(french_text):
+    """Convert French to english"""
+    english_text = language_translator.translate(
+        text=french_text,
+        model_id='fr-en').get_result()
+    return english_text['translations'][0]['translation']
